50a8ea6a9e7ef80887c739e29bdc46da57494395
info-t126 <162181968+info-t126@users.noreply.github.com>
2024-03-05 02:05:53 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,26 @@
+let 結束時間 = 0
+let 開始時間 = 0
+player.onChat("score", function (num1) {
+    if (num1 >= 1 && num1 <= 4) {
+        player.execute(
+        "scoreboard players set @s ShowData " + num1
+        )
+    } else {
+        player.execute(
+        "scoreboard players set @s ShowData 9"
+        )
+    }
+})
+player.onTellCommand("end", function () {
+    gameplay.title(mobs.target(ALL_PLAYERS), "分數計算完畢", "")
+    結束時間 = gameplay.timeQuery(GAME_TIME)
+    player.say("執行時間：" + Math.trunc((結束時間 - 開始時間) / 1200) + "分" + Math.trunc((結束時間 - 開始時間) / 20 % 60) + "秒")
+})
+player.onChat("run", function () {
+    開始時間 = gameplay.timeQuery(GAME_TIME)
+    gameplay.title(mobs.target(ALL_PLAYERS), "開始計算分數", "")
+    player.teleport(world(-80, 35, -10))
+    player.execute(
+    "tag @s add calcScore"
+    )
+})
